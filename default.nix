7621cd0e8b1ce4b00a09c410bd6098bbbae194d5
rexim <reximkut@gmail.com>
2019-10-17 05:58:29 +0700

    Upgrade development Nim to 1.0

--- default.nix
+++ default.nix
@@ -1,16 +1,18 @@
-with import <nixpkgs> {}; {
-    boomerEnv = stdenv.mkDerivation {
-        name = "boomer-env";
-        buildInputs = [ stdenv
-                        gcc
-                        gdb
-                        pkgconfig
-                        nim
-                        xorg.libX11
-                        libGL
-                        libGLU
-                        freeglut
-                      ];
-        LD_LIBRARY_PATH="/run/opengl-driver/lib;${xorg.libX11}/lib/;${libGL}/lib/;${libGLU}/lib;${freeglut}/lib";
-    };
+with import <nixpkgs> {}; let
+  nim_1_0 = callPackage ./overlay/nim_1_0.nim {};
+in rec {
+  boomerEnv = stdenv.mkDerivation {
+    name = "boomer-env";
+    buildInputs = [ stdenv
+                    gcc
+                    gdb
+                    pkgconfig
+                    nim_1_0
+                    xorg.libX11
+                    libGL
+                    libGLU
+                    freeglut
+                  ];
+    LD_LIBRARY_PATH="/run/opengl-driver/lib;${xorg.libX11}/lib/;${libGL}/lib/;${libGLU}/lib;${freeglut}/lib";
+  };
 }
