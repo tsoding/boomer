81834893fc150a38f15119ba9ca1b854411957ad
rexim <reximkut@gmail.com>
2019-11-21 00:29:21 +0700

    (#55) make flashlight radius scale according to cameraScale

--- src/frag.glsl
+++ src/frag.glsl
@@ -6,11 +6,12 @@ uniform vec2 cursorPos;
 uniform vec2 windowSize;
 uniform float flShadow;
 uniform float flRadius;
+uniform float cameraScale;
 
 void main()
 {
     vec4 cursor = vec4(cursorPos.x, windowSize.y - cursorPos.y, 0.0, 1.0);
     color = mix(
         texture(tex, texcoord), vec4(0.0, 0.0, 0.0, 0.0),
-        length(cursor - gl_FragCoord) < flRadius ? 0.0 : flShadow);
+        length(cursor - gl_FragCoord) < (flRadius * cameraScale) ? 0.0 : flShadow);
 }
