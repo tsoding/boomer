72ef2e66e70be0a88837b73d5b1ec6947073fd94
rexim <reximkut@gmail.com>
2019-11-21 01:20:03 +0700

    Make it possible to "zoom backwards"

--- src/navigation.nim
+++ src/navigation.nim
@@ -22,7 +22,7 @@ proc world*(camera: Camera, v: Vec2f): Vec2f =
 proc update*(camera: var Camera, config: Config, dt: float, mouse: Mouse, image: Image) =
   if abs(camera.deltaScale) > 0.5:
     # TODO(#48): camera position adjustment doesn't work anymore
-    camera.scale = max(camera.scale + camera.delta_scale * dt, 1.0)
+    camera.scale = max(camera.scale + camera.delta_scale * dt, 0.01)
     camera.delta_scale -= sgn(camera.delta_scale).float * config.scale_friction * dt
 
   if not mouse.drag and (camera.velocity.length > VELOCITY_THRESHOLD):
