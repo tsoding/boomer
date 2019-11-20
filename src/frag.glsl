467c23b77170e2c3f99315c71cc6673d5509a38c
rexim <reximkut@gmail.com>
2019-11-20 23:55:07 +0700

    (#55) Initial flashlight effect implementation

--- src/frag.glsl
+++ src/frag.glsl
@@ -2,7 +2,18 @@
 out mediump vec4 color;
 in mediump vec2 texcoord;
 uniform sampler2D tex;
+uniform vec2 cursorPos;
+uniform vec2 windowSize;
+
+const float FLASHLIGHT_RADIUS = 200.0;
+
 void main()
 {
-    color = texture(tex, texcoord);
+    vec4 cursor = vec4(cursorPos.x, windowSize.y - cursorPos.y, 0.0, 1.0);
+    float f = 0.8;
+    if (length(cursor - gl_FragCoord) < FLASHLIGHT_RADIUS) {
+        color = texture(tex, texcoord);
+    } else {
+        color = texture(tex, texcoord) - vec4(f, f, f, 0.0);
+    }
 }
